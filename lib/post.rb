60f4be06eef82a750ce0b5ad0f6e0e39e7f51bbe
Valeriy <vel4568@mail.ru>
2022-10-05 17:21:13 +0300

    version SQLite3

---
+++ lib/post.rb
@@ -0,0 +1,125 @@
+require 'sqlite3'
+
+class Post
+
+  @@SQLITE_DB_FILE = 'notepad.sqlite'
+
+  def self.post_types
+    { "Memo" => Memo, "Task" => Task, "Link" => Link }
+  end
+
+  def self.create(type)
+    return post_types[type].new
+  end
+
+  def self.find(limit, type, id)
+    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
+
+    #1.конкретная запись
+    if !id.nil?
+      db.results_as_hash = true
+
+      result = db.execute("SELECT * FROM posts WHERE rowid = ?", id)
+
+      # result = result[0] if result.is_a? Array
+
+      db.close
+
+      if result.empty?
+        abort "Такой id #{id} не найден в базе :("
+      else
+        result = result[0]
+        post = create(result['type'])
+
+        post.load_data(result)
+        return post
+      end
+
+    else
+      db.results_as_hash = false
+
+      query ="SELECT rowid, * FROM posts "
+      query += "WHERE type = :type " unless type.nil?
+      query += "ORDER BY rowid DESC "
+      query += "LIMIT :limit "  unless limit.nil?
+
+      statement = db.prepare(query)
+
+      statement.bind_param("type", type)  unless type.nil?
+      statement.bind_param("limit", limit)  unless limit.nil?
+
+      result = statement.execute!
+
+      statement.close
+
+      db.close
+
+      return  result
+
+    end
+
+  end
+
+  def initialize
+    @created_at = Time.now
+    @text = nil
+  end
+
+  def read_from_console
+
+  end
+
+  def to_string
+
+  end
+
+  def save
+    file = File.new(file_path, "w:UTF-8")
+
+    for item in to_string do
+      file.puts(item)
+    end
+
+    file.close
+  end
+
+  def file_path
+    current_path = File.dirname(__FILE__)
+
+    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+
+    return (current_path + "/" + file_name)
+  end
+
+  def save_to_db
+    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
+    db.results_as_hash = true
+
+    db.execute(
+      "INSERT INTO posts (" +
+        to_db_hash.keys.join(",") +
+        ")" +
+        " VALUES (" +
+        ('?,' * to_db_hash.keys.size).chomp(',') +
+        ")",
+      to_db_hash.values
+    )
+
+    insert_row_id = db.last_insert_row_id
+
+    db.close
+
+    return insert_row_id
+  end
+
+  def to_db_hash
+    {
+      'type' => self.class.name,
+      'created_at' => @created_at.to_s
+    }
+  end
+
+  def load_data(data_hash)
+    @created_at = Time.parse(data_hash['created_at'])
+  end
+end
---
+++ read.rb
@@ -0,0 +1,65 @@
+# encoding: UTF-8
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+require_relative 'lib/post'
+require_relative 'lib/link'
+require_relative 'lib/memo'
+require_relative 'lib/task'
+
+require 'optparse'
+
+# Все наши опции будут записаны сюда
+options = {}
+
+# Заведем нужные нам опции
+OptionParser.new do |opt|
+  # Этот текст будет выводиться, когда программа запущена с опцией -h
+  opt.banner = 'Usage: read.rb [options]'
+
+  # Вывод в случае, если запросили help
+  opt.on('-h', 'Prints this help') do
+    puts opt
+    exit
+  end
+
+  # Опция --type будет передавать тип поста, который мы хотим считать
+  opt.on('--type POST_TYPE', 'какой тип постов показывать ' \
+         '(по умолчанию любой)') { |o| options[:type] = o }
+
+  # Опция --id передает номер записи в базе данных (идентификатор)
+  opt.on('--id POST_ID', 'если задан id — показываем подробно ' \
+         ' только этот пост') { |o| options[:id] = o }
+
+  # Опция --limit передает, сколько записей мы хотим прочитать из базы
+  opt.on('--limit NUMBER', 'сколько последних постов показать ' \
+         '(по умолчанию все)') { |o| options[:limit] = o }
+
+  # В конце у только что созданного объекта класс OptionParser вызываем
+  # метод parse, чтобы он заполнил наш хэш options в соответствии с правилами.
+end.parse!
+
+result = Post.find(options[:limit], options[:type], options[:id])
+
+if result.is_a? Post
+  puts "Запись #{result.class.name}, id = #{options[:id]}"
+  result.to_string.each do |line|
+    puts line
+  end
+else # покажем табоицу результатов
+  print "| id\t| @type\t| @created_at\t\t\t| @text\t\t\t| @url\t\t| @due_date \t "
+  result.each do |row|
+    puts
+
+    row.each do |element|
+      print "| #{element.to_s.delete("\\n\\r")[0..40]}"
+    end
+  end
+
+end
